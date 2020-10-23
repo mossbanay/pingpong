266b080aa24734caa8aab5034495114734f680f3
moss <mossbanay@gmail.com>
2020-10-24 10:43:41 +1100

    Added ability to add nodes and events

--- server/app/routes.py
+++ server/app/routes.py
@@ -1,6 +1,63 @@
+import json
+
+from collections import namedtuple
+
 from app import app
+from flask import request, abort
+
+Node = namedtuple("Node", "ip region_name")
+NODES = []
+
+Event = namedtuple("Event", "timestamp origin_ip dest_ip time")
+EVENTS = []
 
 
 @app.route("/")
 def index():
     return "index"
+
+
+@app.route("/get_nodes")
+def get_nodes():
+    return json.dumps(NODES)
+
+
+@app.route("/get_events")
+def get_events():
+    return json.dumps(EVENTS)
+
+
+@app.route("/register_node")
+def register_node():
+    args = request.args
+
+    if "ip" not in args or "region_name" not in args:
+        abort(400)
+
+    node = Node(args["ip"], args["region_name"])
+    NODES.append(node)
+
+    return ""
+
+
+@app.route("/register_event")
+def register_event():
+    args = request.args
+
+    if (
+        "timestamp" not in args
+        or "origin_ip" not in args
+        or "dest_ip" not in args
+        or "time" not in args
+    ):
+        abort(400)
+
+    event = Event(args["timestamp"], args["origin_ip"], args["dest_ip"], args["time"])
+    EVENTS.append(event)
+
+    return ""
+
+
+@app.errorhandler(400)
+def bad_request(_):
+    return "400 error", 400
