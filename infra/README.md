1b96c9c06ca005f6dee003fdbe5bb3cb5c9ed7a8
moss <mossbanay@gmail.com>
2020-10-25 18:49:57 +1100

    create server instance

---
+++ infra/README.md
@@ -0,0 +1,8 @@
+# Infrastructure
+## How to run
+
+``` sh
+terraform init
+terraform apply
+ansible-playbook -i inventory playbook.yml
+```
--- infra/main.tf
+++ infra/main.tf
@@ -8,12 +8,20 @@ module "client-eu-west-1" {
   region = "eu-west-1"
 }
 
+module "server-ap-southeast-2" {
+  source = "./client"
+  region = "ap-southeast-2"
+}
+
 resource "local_file" "AnsibleInventory" {
   content = templatefile("inventory.tmpl",
     {
-      ip_addrs = [
+      client_ip_addrs = [
         module.client-ap-southeast-2.ip,
         module.client-eu-west-1.ip,
+      ],
+      server_ip_addrs = [
+        module.server-ap-southeast-2.ip,
       ]
   })
   filename        = "inventory"
